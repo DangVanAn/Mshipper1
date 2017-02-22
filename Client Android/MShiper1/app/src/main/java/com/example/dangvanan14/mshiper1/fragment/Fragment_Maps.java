5e72656638ac9f697a93070244adf8a08526f3b1
DESKTOP-1ABEOTJ\dangvanan14 <dangvanan14@gmail.com>
2017-02-22 16:54:16 +0700

    add fragment google maps  - ver 1

---
+++ Client Android/MShiper1/app/src/main/java/com/example/dangvanan14/mshiper1/fragment/Fragment_Maps.java
@@ -0,0 +1,17 @@
+package com.example.dangvanan14.mshiper1.fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.dangvanan14.mshiper1.R;
+
+/**
+ * Created by dangvanan14 on 2/22/2017.
+ */
+
+public class Fragment_Maps extends FragmentActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_maps);
+    }
+}
